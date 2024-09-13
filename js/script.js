88b56ca52eea79563686b0e37a650abda6f45834
miyazatoakira <augusto.akira08@hotmail.com>
2024-09-12 22:23:19 -0300

    feat: Reiniciar jogo, apos morte

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const gameOverText = document.querySelector(".game-over-text");
 let jumping = false; // Variável Booleana
+let loop;
 
 const jump = () => {
   if (jumping == false) {
@@ -14,25 +16,57 @@ const jump = () => {
   }
 };
 
-const loop = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
+const iniciarJogo = () => {
+  console.log("Iniciado");
+  pipe.style.animation = "pipe-animation 1.5s infinite linear";
+  mario.src = "./images/mario.gif";
+  mario.style.width = "150px";
+  mario.style.marginLeft = "0px";
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+  loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+      .getComputedStyle(mario)
+      .bottom.replace("px", "");
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+      pipe.style.animation = "none";
+      pipe.style.left = `${pipePosition}px`;
 
-    mario.src = "./images/game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "50px";
+      mario.style.animation = "none";
+      mario.style.bottom = `${marioPosition}px`;
 
-    clearInterval(loop);
+      mario.src = "./images/game-over.png";
+      mario.style.width = "75px";
+      mario.style.marginLeft = "50px";
+
+      gameOverText.style.opacity = "100%";
+
+      clearInterval(loop);
+    }
+  }, 10);
+};
+
+const reiniciar = () => {
+  clearInterval(loop);
+  gameOverText.style.opacity = "0%";
+  mario.src = "./images/mario.gif";
+  mario.style.width = "150px";
+  mario.style.marginLeft = "0px";
+  mario.style.bottom = "0px";
+  mario.style.animation = "";
+  pipe.style.left = "unset";
+  iniciarJogo();
+};
+
+// Chamada de Funções para Início e Reset do jogo
+
+iniciarJogo(); // Função p/ efetivamente iniciar Jogo
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Enter") {
+    reiniciar();
   }
-}, 10);
+});
 
 document.addEventListener("keydown", jump);
